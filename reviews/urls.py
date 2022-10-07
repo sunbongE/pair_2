cd45dfd562c7f3524e16d10eb161498173807ad1
taehopark <qkrxogh7@gmail.com>
2022-10-07 11:47:39 +0900

    crud

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'reviews'
+app_name = "reviews"
 
 urlpatterns = [
-    path('reviews/', views.reviews, name='reviews'),
-    path('create/', views.create, name='create'),
+    path("reviews/", views.reviews, name="reviews"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/delete", views.delete, name="delete"),
+    path("<int:pk>/update", views.update, name="update"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,61 @@
 from django.shortcuts import render, redirect
+
+from reviews.forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
+
 def reviews(request):
-    
-    return render(request, 'reviews/reviews.html')
+    reviews = Review.objects.all().order_by("-pk")
+
+    context = {"reviews": reviews}
+
+    return render(request, "reviews/reviews.html", context)
+
 
 def create(request):
-    return render(request, 'reviews/create.html')
+    if request.method == "POST":
+
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:reviews")
+    else:
+        review_form = ReviewForm()
+
+    context = {"review_form": review_form}
+
+    return render(request, "reviews/create.html", context)
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    context = {"review": review}
+
+    return render(request, "reviews/detail.html", context)
+
+
+def delete(request, pk):
+
+    review = Review.objects.get(pk=pk)
+    review.delete()
+    return redirect("reviews:reviews")
+
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, instance=review)
+
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:detail", review.pk)
+    else:
+        review_form = ReviewForm(instance=review)
+
+    context = {"review_form": review_form}
+
+    return render(request, "reviews/update.html", context)
